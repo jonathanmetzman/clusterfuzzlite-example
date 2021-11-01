ee54f98385f2e072a057f9a382aaec2ecdc0f64c
Jonathan Metzman <metzman@chromium.org>
2021-11-01 11:28:21 -0700

    Initial commit

---
+++ example_fuzzer.cpp
@@ -0,0 +1,23 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+#include "my_api.h"
+
+#include <string>
+#include <stdlib.h>
+
+// Simple fuzz target for DoStuff().
+// See http://libfuzzer.info for details.
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  if (size < 2) {
+    return 0;
+  }
+  if (data[1] != 'b') {
+    return 0;
+  }
+  std::string str(reinterpret_cast<const char *>(data), size);
+  DoStuff(str);  // Disregard the output.
+  if (data[0] == 'a') {
+    return 0;
+  }
+  return 0;
+}
