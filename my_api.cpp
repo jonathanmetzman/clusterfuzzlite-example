61d5af9290ec33ef47756882d91113ddde49b91e
Yang Fu <st180586@stud.uni-stuttgart.de>
2023-05-28 21:20:27 +0200

    change omg to mg

--- my_api.cpp
+++ my_api.cpp
@@ -22,7 +22,7 @@ size_t DoStuff(const std::string &str) {
     Idx++;
   if (str.find("ouch") != std::string::npos)
     Idx++;
-  if (str.find("omg") != std::string::npos)
+  if (str.find("mg") != std::string::npos)
     Idx++;
   return Vec[Idx];
 }
